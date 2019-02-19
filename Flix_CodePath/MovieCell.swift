cffd7266e56f4170692f2a5f3427bcc7767ba2b5
Om Prakaash <omsuseela@gmail.com>
2019-02-18 16:15:03 -0800

    Working Movies App with scrollable table view populated with images

---
+++ Flix_CodePath/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Flix_CodePath
+//
+//  Created by Om Prakaash on 2/12/19.
+//  Copyright © 2019 Om Prakaash. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
